db888dd3334a23917a66d343ceb710a7275335a3
KalsonSaint <saintkalson@gmail.com>
2020-07-30 16:15:11 +0100

    using Context API & Provider State

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,29 +1,8 @@
 import React, { Component } from 'react'
 import Contact from './Contact'
+import { Consumer } from '../Context'
 
 class Contacts extends Component{
-    state = {
-        contacts: [
-            {
-                id: 1,
-                name: 'Kalson Steve',
-                email: 'kalsonst@gmail.com',
-                phone: '111-111-1111'
-            },
-            {
-                id: 2,
-                name: 'Stephen Kalson',
-                email: 'kalsonst@gmail.com',
-                phone: '222-222-2222'
-            },
-            {
-                id: 3,
-                name: 'Kalson Saint',
-                email: 'kalsonst@gmail.com',
-                phone: '333-333-3333'
-            }
-        ]
-    }
 
     deleteContact = (id) => {
         const { contacts } = this.state;
@@ -35,18 +14,23 @@ class Contacts extends Component{
     };
 
     render() {
-        //destructuring object
-        const { contacts } = this.state;
         return(
-            <React.Fragment>
-                {contacts.map(contact => 
-                    <Contact 
-                        key={contact.id} 
-                        contact={contact} 
-                        deleteClickHandler = {this.deleteContact.bind(this, contact.id)}
-                    />
-                )}
-            </React.Fragment>
+            <Consumer>
+                {value => {
+                    const { contacts } = value;
+                    return(
+                        <React.Fragment>
+                            {contacts.map(contact => 
+                                <Contact 
+                                    key={contact.id} 
+                                    contact={contact} 
+                                    deleteClickHandler = {this.deleteContact.bind(this, contact.id)}
+                                />
+                            )}
+                        </React.Fragment>
+                    );
+                }}
+            </Consumer>
         )
     }
 }
