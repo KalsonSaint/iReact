ac503f1ff7c32de029c26783e75ddc8d5d6d49b9
KalsonSaint <saintkalson@gmail.com>
2020-08-22 06:19:22 +0100

    Updates

---
+++ precache-manifest.005b07b3008cddebbe100c89de8d715c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8fd087c3dd11d6c67ec157d3bde0d4",
+    "url": "/iReact/index.html"
+  },
+  {
+    "revision": "682532cae3f8ae068f03",
+    "url": "/iReact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "efc3d7fdb54c19ec8dfa",
+    "url": "/iReact/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "682532cae3f8ae068f03",
+    "url": "/iReact/static/js/2.868cd83f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/iReact/static/js/2.868cd83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc3d7fdb54c19ec8dfa",
+    "url": "/iReact/static/js/main.5e5454e4.chunk.js"
+  },
+  {
+    "revision": "db34ac85888fb71e90a6",
+    "url": "/iReact/static/js/runtime-main.0e6962a3.js"
+  }
+]);
